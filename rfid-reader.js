e8e507dc16ab4f719fc6f91093460db1e798da5e
nerdgrass <arboerstler@gmail.com>
2013-11-14 17:19:51 -0500

    Added jQuery via Bower, form captures input to console.log on submit.

--- rfid-reader.js
+++ rfid-reader.js
@@ -1,1 +1,9 @@
-document.forms['usb-input-form'].elements['usb-input'].focus();
+// document.forms['usb-input-form'].elements['usb-input'].focus();
+
+$(function() { //shorthand document.ready function
+    $('#foo').on('submit', function(e) { //use on if jQuery 1.7+
+        e.preventDefault();  //prevent form from submitting
+        var data = $("#foo :input").serializeArray();
+        console.log(data); //use the console for debugging, F12 in Chrome, not alerts
+    });
+});
